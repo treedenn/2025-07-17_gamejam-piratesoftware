1089b5ca1396cd5ab2c4696a2b276fda12184b8e
treeden <petersen.dnns@gmail.com>
2025-07-18 21:49:19 +0200

    feat(collision): add colission to the tileset

--- player/player.gd
+++ player/player.gd
@@ -3,55 +3,57 @@ class_name Player
 
 const TILE_SIZE = 16
 
-var _movement_cooldown := 0.25
-var _movement_cooldown_timer := 0.0
-
-var _velocity := Vector2.ZERO
-
-var _anim_speed := 0.25
+@export var tilemap: TileMapLayer
 
 var _moving := false
+var _movement_cooldown := 0.25
+var _movement_cooldown_timer := 0.0
+var _velocity := Vector2.ZERO
+
+@onready var raycast := $RayCast2D
+
+func _init() -> void:
+	floor_snap_length = TILE_SIZE
 
 func _ready() -> void:
-	position = position.snapped(Vector2.ONE * TILE_SIZE) - Vector2(8, 8)
+	position = position.snapped(Vector2.ONE * TILE_SIZE) + Vector2.ONE * 8
 
 func _unhandled_input(event: InputEvent) -> void:
 	var hdir := Input.get_axis("player_left", "player_right")
 	var vdir := Input.get_axis("player_up", "player_down")
+	_velocity = Vector2i(hdir, vdir)
 	
-	# improvement? check the latest key, if its W/S, set hdir to 0, if A/D set vdir to 0.
+	# player is pressing both directions, priotize left/right
+	if _velocity.x != 0 && _velocity.y != 0:
+		_velocity.y = 0
 	
-	if hdir != 0 && vdir != 0:
-		vdir = 0
-
-	_velocity = Vector2(hdir, vdir)
+	# do not call below if an actual player movement action has been pressed
+	if !_velocity.is_zero_approx():
+		if canMove():
+			_moving = true
+			move(_velocity)
 
 func _process(delta: float) -> void:
-	if !canMove():
-		_movement_cooldown_timer += delta
-		
-func _physics_process(delta: float) -> void:
-	if canMove() && _velocity != Vector2.ZERO:
-		move(_velocity)
+	if _movement_cooldown_timer > 0:
+		_movement_cooldown_timer -= delta
 
 func move(vel: Vector2):
+	var animation_speed := .25
 	var tween = create_tween()
-	tween.tween_property(self, "position", position + vel * TILE_SIZE, _anim_speed).set_trans(Tween.TRANS_SINE)
-	# Keeping this here for future references until no longer necessary
-	# TODO: Remove
-	#position += vel * TILE_SIZE
-	
-	_moving = true
-	_movement_cooldown_timer = 0
+	tween.tween_property(self, "position", position + vel * TILE_SIZE, animation_speed).set_trans(Tween.TRANS_SINE)
 	
 	await tween.finished
 	
 	_moving = false
-	
-	
+	_movement_cooldown_timer = _movement_cooldown
 	
 func canMove() -> bool:
 	if _moving:
 		return false
 		
-	return _movement_cooldown_timer >= _movement_cooldown
+	# collision detection against the tilemap physical layer
+	raycast.target_position = _velocity * TILE_SIZE
+	if raycast.is_colliding():
+		return false
+		
+	return _movement_cooldown_timer <= 0
