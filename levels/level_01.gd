d3782ef2445dd615357d6a2f9d2f84471e422ed1
Graceland0037 <patrick.r.shkodra@hotmail.com>
2025-07-30 21:12:58 +0200

    Set Receiver? Nah, add receiver bruh

--- levels/level_01.gd
+++ levels/level_01.gd
@@ -31,13 +31,13 @@ func _exit_tree() -> void:
 		
 func _wired():
 	#Switches inputs into XOrGate and XOrGate inputs into AndGate
-	m_grp2_switch1.m_transmitter.set_receiver(m_grp2_xor_gate.m_receiver1)
-	m_grp2_switch2.m_transmitter.set_receiver(m_grp2_xor_gate.m_receiver2)
-	m_grp2_xor_gate.m_transmitter.set_receiver(m_grp1_and_gate.m_receiver1)
+	m_grp2_switch1.m_transmitter.add_receiver(m_grp2_xor_gate.m_receiver1)
+	m_grp2_switch2.m_transmitter.add_receiver(m_grp2_xor_gate.m_receiver2)
+	m_grp2_xor_gate.m_transmitter.add_receiver(m_grp1_and_gate.m_receiver1)
 	
 	#Switch inputs into AndGate and AndGate inputs in Diode
-	m_grp1_switch1.m_transmitter.set_receiver(m_grp1_and_gate.m_receiver2)
-	m_grp1_and_gate.m_transmitter.set_receiver(m_diode.m_receiver)
+	m_grp1_switch1.m_transmitter.add_receiver(m_grp1_and_gate.m_receiver2)
+	m_grp1_and_gate.m_transmitter.add_receiver(m_diode.m_receiver)
 
 func _on_level_completed():
 	next_level_menu.level_completed = true
